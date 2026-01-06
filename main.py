920855ad442d001286278295b0c159ff602502e5
abhishekm-bit <abhishek.m@glideclouds.com>
2026-01-06 17:10:28 +0530

    Frontend Connected to the Backend

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 from fastapi import FastAPI
 from routes import router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+# ✅ ADD THIS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],   # allow all origins (for local dev)
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(router)
 
 @app.get("/")
+
 def root():
     return {"status": "API running"}
